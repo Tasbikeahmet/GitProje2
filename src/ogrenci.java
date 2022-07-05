af04c464fc4cd5837e2d17470959b4a29828791a
ahmet <oyuntaso@gmail.com>
2022-07-05 16:44:07 +0600

    ogrenci classi ikinci bolum yapildi

--- src/ogrenci.java
+++ src/ogrenci.java
@@ -1,6 +1,8 @@
 public class ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba dunya" );
+
+        System.out.println("Merhaba dunya 2");
+        System.out.println("merhaba dunya 3");
 
     }
 }
